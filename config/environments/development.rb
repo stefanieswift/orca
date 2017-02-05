3f7a559fe9b27ddd9dc140eff34c7ed575015cfc
Stefanie Swift <swift.stefanie@gmail.com>
2017-02-04 19:37:51 -0800

    add social service -- need to consider the constraints of instragram api and how to continue from here.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,8 @@ Rails.application.configure do
 
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
-    config.action_controller.perform_caching = true
+    config.a
+    ction_controller.perform_caching = true
 
     config.cache_store = :memory_store
     config.public_file_server.headers = {
---
+++ config/initializers/instagram.rb
@@ -0,0 +1,5 @@
+require "instagram"
+Instagram.configure do |config|
+  config.client_id = "YOUR CLIENT ID"
+  config.access_token = "YOUR ACCESS TOKEN"
+end
